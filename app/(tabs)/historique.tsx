1285a32864cbc5ba9f51af89b10a51202f8bc253
Shiroi <mattheo55.b@gmail.com>
2025-10-20 15:45:57 +0200

    ⚙️ Add Safe Area view

--- app/(tabs)/historique.tsx
+++ app/(tabs)/historique.tsx
@@ -1,11 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function historique() {
   return (
-    <View>
+    <SafeAreaView>
       <Text>historique</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 

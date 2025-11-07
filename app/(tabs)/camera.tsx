32f51ea2352c8d19bb18c06396eb6e2bc33b2670
Shiroi <mattheo55.b@gmail.com>
2025-11-07 10:16:14 +0100

    🔥Correction Caméra
    Co-Authored-By: Thibaut <197876887+Dibeau19@users.noreply.github.com>
    Co-Authored-By: Maxence Venner <132772531+ISmeliodasNT@users.noreply.github.com>

--- app/(tabs)/camera.tsx
+++ app/(tabs)/camera.tsx
@@ -1,11 +1,10 @@
-import { Camera, CameraType } from 'expo-camera';
-import React, { useEffect, useRef, useState } from 'react';
+import { Camera, CameraType, CameraView } from 'expo-camera';
+import React, { useEffect, useState } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function CameraScreen() {
   const [hasPermission, setHasPermission] = useState<boolean | null>(null);
-  const [type, setType] = useState<CameraType>(CameraType.back);
-  const cameraRef = useRef<CameraTypeRef | null>(null); // ✅ Type correct ici
+  const [type, setType] = useState<CameraType>("back");
 
   useEffect(() => {
     (async () => {
@@ -17,13 +16,13 @@ export default function CameraScreen() {
   if (hasPermission === null) return <Text>Demande de permission...</Text>;
   if (hasPermission === false) return <Text>Permission refusée à la caméra.</Text>;
 
-  const switchCamera = () => {
-    setType(type === CameraType.back ? CameraType.front : CameraType.back);
-  };
+ function switchCamera() {
+  setType((prevType) => (prevType === "back" ? "front" : "back"));
+}
 
   return (
     <View style={styles.container}>
-      <Camera style={styles.camera} type={type} ref={cameraRef} />
+      <CameraView style={styles.camera} facing={type} />
       <View style={styles.buttonContainer}>
         <Button title="🔄 Changer caméra" onPress={switchCamera} />
       </View>
